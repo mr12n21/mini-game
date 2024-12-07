b772736a0f58fdb6008de483108fb2b6c55af64d
Marek Brož <marek.broz@student.sps-cl.cz>
2024-12-08 00:54:43 +0100

    update game.cs

--- game.cs
+++ game.cs
@@ -1,126 +1,156 @@
 ﻿using System;
-using System.Threading;
+using System.Drawing;
+using System.Windows.Forms;
 
-class Program
+public class PongGame : Form
 {
-    static void Main()
+    private int PaddleWidth, PaddleHeight, BallSize;
+    private int leftPaddleY, rightPaddleY, ballX, ballY, ballSpeedX, ballSpeedY;
+    private int leftScore = 0, rightScore = 0;
+    private int paddleSpeed = 10;
+    private const int winScore = 10;
+    private bool leftUp, leftDown, rightUp, rightDown;
+    private Timer gameTimer;
+    private int maxSpeed = 20;
+
+    public PongGame()
     {
-        Console.CursorVisible = false;
-        const int screenHeight = 20;
-        const int screenWidth = 40;
-        const int groundHeight = 2;
+        ballSpeedX = 5;
+        ballSpeedY = 5;
 
-        int playerX = 5;
-        int playerY = screenHeight - groundHeight - 1;
-        bool isJumping = false;
-        int jumpHeight = 5;
-        int jumpCounter = 0;
+        this.Text = "Pong Game";
+        this.Size = new Size(800, 600);
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.MaximizeBox = false;
+        this.StartPosition = FormStartPosition.CenterScreen;
 
-        Random random = new Random();
-        int obstacleX = screenWidth - 1;
-        int obstacleY = screenHeight - groundHeight - 1;
+        this.KeyDown += new KeyEventHandler(OnKeyDown);
+        this.KeyUp += new KeyEventHandler(OnKeyUp);
 
-        int score = 0;
-        bool gameOver = false;
+        gameTimer = new Timer();
+        gameTimer.Interval = 16;
+        gameTimer.Tick += new EventHandler(OnGameTick);
+        gameTimer.Start();
 
-        void DrawPlayer()
+        this.Resize += new EventHandler(OnResize);
+        OnResize(this, EventArgs.Empty);
+    }
+
+    private void OnResize(object sender, EventArgs e)
+    {
+        float formWidth = this.ClientSize.Width;
+        float formHeight = this.ClientSize.Height;
+
+        PaddleHeight = (int)(formHeight * 0.2);
+        PaddleWidth = (int)(formWidth * 0.02);
+
+        BallSize = (int)(formWidth * 0.03);
+
+        ballSpeedX = (int)(Math.Min(formWidth / 50, maxSpeed));
+        ballSpeedY = (int)(Math.Min(formHeight / 50, maxSpeed));
+
+        leftPaddleY = (this.ClientSize.Height - PaddleHeight) / 2;
+        rightPaddleY = (this.ClientSize.Height - PaddleHeight) / 2;
+        ballX = this.ClientSize.Width / 2;
+        ballY = this.ClientSize.Height / 2;
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.W) leftUp = true;
+        if (e.KeyCode == Keys.S) leftDown = true;
+        if (e.KeyCode == Keys.Up) rightUp = true;
+        if (e.KeyCode == Keys.Down) rightDown = true;
+    }
+
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == Keys.W) leftUp = false;
+        if (e.KeyCode == Keys.S) leftDown = false;
+        if (e.KeyCode == Keys.Up) rightUp = false;
+        if (e.KeyCode == Keys.Down) rightDown = false;
+    }
+
+    private void OnGameTick(object sender, EventArgs e)
+    {
+        if (leftUp && leftPaddleY > 0) leftPaddleY -= paddleSpeed;
+        if (leftDown && leftPaddleY < this.ClientSize.Height - PaddleHeight) leftPaddleY += paddleSpeed;
+        if (rightUp && rightPaddleY > 0) rightPaddleY -= paddleSpeed;
+        if (rightDown && rightPaddleY < this.ClientSize.Height - PaddleHeight) rightPaddleY += paddleSpeed;
+
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if (ballY <= 0 || ballY >= this.ClientSize.Height - BallSize)
         {
-            Console.SetCursorPosition(playerX, playerY);
-            Console.Write("C#");
+            ballSpeedY = -ballSpeedY;
         }
 
-        void DrawGround()
+        if (ballX <= PaddleWidth && ballY >= leftPaddleY && ballY <= leftPaddleY + PaddleHeight)
         {
-            for (int x = 0; x < screenWidth; x++)
-            {
-                Console.SetCursorPosition(x, screenHeight - groundHeight);
-                Console.Write("=");
-            }
+            ballSpeedX = -ballSpeedX;
+            IncreaseBallSpeed();
+        }
+        if (ballX >= this.ClientSize.Width - PaddleWidth - BallSize && ballY >= rightPaddleY && ballY <= rightPaddleY + PaddleHeight)
+        {
+            ballSpeedX = -ballSpeedX;
+            IncreaseBallSpeed();
         }
 
-        void DrawObstacle()
+        if (ballX <= 0)
         {
-            Console.SetCursorPosition(obstacleX, obstacleY);
-            Console.Write("|");
+            rightScore++;
+            ResetBall();
+        }
+        if (ballX >= this.ClientSize.Width - BallSize)
+        {
+            leftScore++;
+            ResetBall();
         }
 
-        void ClearPosition(int x, int y)
+        if (leftScore >= winScore || rightScore >= winScore)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write(" ");
+            gameTimer.Stop();
+            MessageBox.Show($"Game Over! {(leftScore >= winScore ? "Left" : "Right")} player wins!", "Game Over");
+            this.Close();
         }
 
-        void UpdateObstacle()
-        {
-            if (obstacleX == playerX && obstacleY == playerY)
-            {
-                gameOver = true;
-            }
+        this.Invalidate();
+    }
 
-            if (obstacleX <= 0)
-            {
-                obstacleX = screenWidth - 1;
-                score++;
-            }
-            else
-            {
-                obstacleX--;
-            }
-        }
+    private void IncreaseBallSpeed()
+    {
+        if (ballSpeedX < maxSpeed && ballSpeedX > 0) ballSpeedX++;
+        if (ballSpeedX > -maxSpeed && ballSpeedX < 0) ballSpeedX--;
+        if (ballSpeedY < maxSpeed && ballSpeedY > 0) ballSpeedY++;
+        if (ballSpeedY > -maxSpeed && ballSpeedY < 0) ballSpeedY--;
+    }
 
-        void HandleJump()
-        {
-            if (isJumping)
-            {
-                if (jumpCounter < jumpHeight)
-                {
-                    ClearPosition(playerX, playerY);
-                    playerY--;
-                    jumpCounter++;
-                }
-                else
-                {
-                    isJumping = false;
-                }
-            }
-            else if (playerY < screenHeight - groundHeight - 1)
-            {
-                ClearPosition(playerX, playerY);
-                playerY++;
-            }
-            else
-            {
-                jumpCounter = 0;
-            }
-        }
+    private void ResetBall()
+    {
+        ballX = this.ClientSize.Width / 2;
+        ballY = this.ClientSize.Height / 2;
+        ballSpeedX = 5;
+        ballSpeedY = 5;
+    }
 
-        while (!gameOver)
-        {
-            Console.Clear();
-            DrawGround();
-            DrawPlayer();
-            DrawObstacle();
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
 
-            if (Console.KeyAvailable)
-            {
-                var key = Console.ReadKey(intercept: true).Key;
-                if (key == ConsoleKey.Spacebar && playerY == screenHeight - groundHeight - 1)
-                {
-                    isJumping = true;
-                }
-            }
+        e.Graphics.FillRectangle(Brushes.Blue, 0, leftPaddleY, PaddleWidth, PaddleHeight);
+        e.Graphics.FillRectangle(Brushes.Blue, this.ClientSize.Width - PaddleWidth, rightPaddleY, PaddleWidth, PaddleHeight);
+        e.Graphics.FillEllipse(Brushes.Red, ballX, ballY, BallSize, BallSize);
 
-            UpdateObstacle();
-            HandleJump();
+        string scoreText = $"{leftScore} - {rightScore}";
+        var font = new Font("Arial", 24);
+        var textSize = e.Graphics.MeasureString(scoreText, font);
+        e.Graphics.FillRectangle(Brushes.Black, (this.ClientSize.Width - textSize.Width) / 2 - 10, 10, textSize.Width + 20, textSize.Height + 10);
+        e.Graphics.DrawString(scoreText, font, Brushes.White, (this.ClientSize.Width - textSize.Width) / 2, 10);
+    }
 
-            Console.SetCursorPosition(0, 0);
-            Console.Write($"Score: {score}");
-
-            Thread.Sleep(150);
-        }
-
-        Console.Clear();
-        Console.WriteLine("Game Over!");
-        Console.WriteLine($"Final Score: {score}");
+    public static void Main()
+    {
+        Application.Run(new PongGame());
     }
 }
